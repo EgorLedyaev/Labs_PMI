a37f89186e3b0c6b66a15541ec8fb92a2fb1b172
Egor <40001420+EgorLedyaev@users.noreply.github.com>
2022-10-25 17:13:55 +0400

    Update 4_pm.cpp

--- 1/11/4_pm.cpp
+++ 1/11/4_pm.cpp
@@ -2,57 +2,122 @@
 //Не используя string, а используя массив символов
 
 #include <iostream>
-#include <Windows.h>
 #include <string>
-#include <cstring>
-#include <algorithm>
-
+#include<fstream>
+#include<vector>
+#include<Windows.h>
 
 using namespace std;
 
-//Функция для принятия и проверки длины слова
-int check_n(){
-    while (true){
-        cout <<"Введите длину слова:";
-        string n;
-        //ввод с помощью getlines
-        getline(cin, n);
-        //проверяем, что в строке содержится только число int
-        if (n.find_first_not_of("0123456789") != string::npos || n.empty()){
-            cout << "Неверный формат числа!" << endl;
+//функция для принятия строки из файла в вектор символов и принятия длины слова для удаления из файла
+void input(vector<char>& str, int& len, ifstream& fin)
+{
+    char c;
+    while (fin.get(c))
+    {
+        if (c == '\n')
+        {
+            break;
         }
-        else{
-            //проверяем, что число не отрицательное
-            if (stof(n) < 2){
-                cout << "Число не может быть отрицательным или меньше 1!" << endl;
-            }
-            else{
-                return stoi(n);
-            }
-        }
+        str.push_back(c);
+    }
+    fin >> len;
+}
+
+//функция для записи строки в файл
+void writeStringToFile(string fileName, vector<char> str)
+{
+	ofstream fout(fileName);
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		fout << str[i];
+		i++;
+	}
+}
+
+//функция для удаления слова заданной длины
+void deleteWordFromStr(vector<char> &str, int wordLength)
+{
+	int i = 0;
+	int count = 0;
+	while (str[i] != '\0')
+	{
+		//проверка на пробел
+		if (str[i] == ' ' or str[i] == ',' or str[i] == '.')
+		{
+			//если количество символов в слове равно заданной длине
+			if (count == wordLength)
+			{
+				//удаление слова
+				int j = i;
+				while (str[j] != '\0')
+				{
+					str[j - count - 1] = str[j];
+					j++;
+				}
+				str[j - count - 1] = '\0';
+				i -= count + 1;
+				count = 0;
+
+			}
+			else
+			{
+				count = 0;
+			}
+		}
+		else
+		{
+			count++;
+		}
+		i++;
+	}
+	//проверка последнего слова
+	if (count == wordLength)
+	{
+		int j = i;
+		while (str[j] != '\0')
+		{
+			str[j - count - 1] = str[j];
+			j++;
+		}
+		str[j - count - 1] = '\0';
+	}
+}
+
+//функция для вывода исходной строки
+void printStr(vector<char> str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        cout << str[i];
+        i++;
     }
 }
 
-
+//функция для вывода того что получилось
+void printStr2(vector<char> str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        cout << str[i];
+        i++;
+    }
+}
 int main()
 {
     SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
-    //вводим размер слова
-    int n = check_n();
-    char str[100];
-    cout << "Введите строку: ";
-    cin.ignore();
-    cin.getline(str, 100);
-    char* p = strtok(str, " ");
-    while (p != NULL)
-    {
-        if (strlen(p) != n)
-        {
-            cout << p << " ";
-        }
-        p = strtok(NULL, " ");
-    }
+	SetConsoleOutputCP(1251);
+	ifstream fin("input.txt");
+    vector<char> str;
+    int len;
+    input(str, len, fin);
+    printStr(str);
+    deleteWordFromStr(str, len);
+    cout << endl;
+    printStr2(str);
+    writeStringToFile("output.txt", str);
     return 0;
 }
